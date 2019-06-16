73fc5459ca01a7a4d231aaa88c98e1e7f203531d
Paulo Alves <front@agencianovi.com.br>
2019-06-15 22:10:15 -0300

    Site finalizado

---
+++ contato.php
@@ -0,0 +1,52 @@
+<?php include 'components/header.php'; ?>
+
+	<section class="topo-interna contato">
+		<h1>CONTATO</h1>
+	</section><!-- topo-interna -->
+
+	<section class="interna contato">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 col-sm-6">
+					<form>
+						<input class="form-control" placeholder="NOME" />
+						<input class="form-control" placeholder="E-MAIL" />
+						<input class="form-control" placeholder="TELEFONE" />
+						<input class="form-control" placeholder="CIDADE" />
+						<button class="btn-azul">ENVIAR</button>
+						<p class="msg-sucesso">MENSAGEM ENVIADA COM SUCESSO!</p>
+						<p class="msg-erro">TENTE NOVAMENTE MAIS TARDE.</p>
+					</form>
+				</div><!-- md-6 -->
+				<div class="col-md-6 col-sm-6">
+					<ul>
+						<li>
+							<span><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+							<h2>R. Pedro Pilato, 360<br /><strong>Umbará, Curitiba/PR</strong></h2>	
+						</li>
+						<li>
+							<span><i class="fa fa-phone" aria-hidden="true"></i></span>
+							<h4>Central de Atendimento:</h4>
+							<h2>41 3348.1151</h2>
+						</li>
+						<li>
+							<span><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+							<h4>Nosso E-mail:</h4>
+							<p>contato@ceramicanichele.com.br</p>
+						</li>
+						<li>
+							<span><i class="fa fa-clock-o" aria-hidden="true"></i></span>
+							<h4>Horário de Atendimento:</h4>
+							<p>Segunda a Sexta das 08h00 às 18h00</p>
+							<p>Sábado das 08h00 às 11h30</p>
+							<p>Domingo fechado</p>
+						</li>
+					</ul>
+				</div><!-- md-6 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- interna -->
+
+	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3598.7112503066505!2d-49.29277988498261!3d-25.581271683718974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dcfeaef4fac13d%3A0x54f407d063b3b829!2sR.+Pedro+Pilato%2C+360+-+Umbar%C3%A1%2C+Curitiba+-+PR%2C+81940-486!5e0!3m2!1spt-BR!2sbr!4v1560619826576!5m2!1spt-BR!2sbr" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+<?php include 'components/footer.php'; ?>
--- produtos.php
+++ produtos.php
@@ -8,19 +8,236 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-3 col-sm-3">
-					<ul>
-						<li>
-							<a href="">
-								TIJOLOS
-							</a>
-						</li>
-					</ul>
+					<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                        <div class="panel panel-default">
+                            <div class="panel-heading" role="tab" id="heading0">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse0" aria-expanded="false" aria-controls="collapse0">TIJOLOS</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                                <div class="panel-body">
+                                    <ul>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    </ul>
+                                </div><!-- panel-body -->
+                            </div><!-- collapseOne -->
+                            <div class="panel-heading" role="tab" id="heading0">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse0" aria-expanded="false" aria-controls="collapse0">BLOCOS</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                                <div class="panel-body">
+                                    <ul>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    </ul>
+                                </div><!-- panel-body -->
+                            </div><!-- collapseOne -->
+                            <div class="panel-heading" role="tab" id="heading0">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse0" aria-expanded="false" aria-controls="collapse0">CANALETAS</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                                <div class="panel-body">
+                                    <ul>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    </ul>
+                                </div><!-- panel-body -->
+                            </div><!-- collapseOne -->
+                            <div class="panel-heading" role="tab" id="heading0">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse0" aria-expanded="false" aria-controls="collapse0">LAJOTAS</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                                <div class="panel-body">
+                                    <ul>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    </ul>
+                                </div><!-- panel-body -->
+                            </div><!-- collapseOne -->
+                            <div class="panel-heading" role="tab" id="heading0">
+                                <h4 class="panel-title">
+                                    <a role="button" data-toggle="collapse" class="accordion-toggle collapsed" data-parent="#accordion" href="#collapse0" aria-expanded="false" aria-controls="collapse0">MINIATURAS</a>
+                                </h4>
+                            </div><!-- panel-heading -->
+                            <div id="collapse0" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading0">
+                                <div class="panel-body">
+                                    <ul>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    	<li><a href="">9 X 14 X 19</a></li>
+                                    </ul>
+                                </div><!-- panel-body -->
+                            </div><!-- collapseOne -->
+                        </div><!-- panel -->
+                    </div><!-- panel-group -->
 				</div><!-- md-3 -->
 				<div class="col-md-9 col-sm-9">
 					<div class="row">
-						<div class="col-md-4 col-sm-4">
+						<div class="col-md-4 col-sm-6">
 							<div class="item">
-								
+								<a href="">
+									<div class="img">
+										<img src="assets/images/produto.png">
+									</div><!-- img -->
+									<div class="content">
+										<h5>TIJOLO</h5>
+										<h2>9 X 14 X 19</h2>
+									</div><!-- content -->
+									<div class="conteudo">
+										<h4>Rendimento:</h4>
+										<ul>
+											<li>
+												<img src="assets/images/icone-rend1.png">
+												<p>33,4<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend2.png">
+												<p>50<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend3.png">
+												<p>1,800<br />kg</p>
+											</li>
+										</ul>
+									</div><!-- conteudo -->
+								</a>
+							</div><!-- item -->
+						</div><!-- md-4 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="item">
+								<a href="">
+									<div class="img">
+										<img src="assets/images/produto.png">
+									</div><!-- img -->
+									<div class="content">
+										<h5>TIJOLO</h5>
+										<h2>9 X 14 X 19</h2>
+									</div><!-- content -->
+									<div class="conteudo">
+										<h4>Rendimento:</h4>
+										<ul>
+											<li>
+												<img src="assets/images/icone-rend1.png">
+												<p>33,4<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend2.png">
+												<p>50<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend3.png">
+												<p>1,800<br />kg</p>
+											</li>
+										</ul>
+									</div><!-- conteudo -->
+								</a>
+							</div><!-- item -->
+						</div><!-- md-4 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="item">
+								<a href="">
+									<div class="img">
+										<img src="assets/images/produto.png">
+									</div><!-- img -->
+									<div class="content">
+										<h5>TIJOLO</h5>
+										<h2>9 X 14 X 19</h2>
+									</div><!-- content -->
+									<div class="conteudo">
+										<h4>Rendimento:</h4>
+										<ul>
+											<li>
+												<img src="assets/images/icone-rend1.png">
+												<p>33,4<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend2.png">
+												<p>50<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend3.png">
+												<p>1,800<br />kg</p>
+											</li>
+										</ul>
+									</div><!-- conteudo -->
+								</a>
+							</div><!-- item -->
+						</div><!-- md-4 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="item">
+								<a href="">
+									<div class="img">
+										<img src="assets/images/produto.png">
+									</div><!-- img -->
+									<div class="content">
+										<h5>TIJOLO</h5>
+										<h2>9 X 14 X 19</h2>
+									</div><!-- content -->
+									<div class="conteudo">
+										<h4>Rendimento:</h4>
+										<ul>
+											<li>
+												<img src="assets/images/icone-rend1.png">
+												<p>33,4<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend2.png">
+												<p>50<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend3.png">
+												<p>1,800<br />kg</p>
+											</li>
+										</ul>
+									</div><!-- conteudo -->
+								</a>
+							</div><!-- item -->
+						</div><!-- md-4 -->
+						<div class="col-md-4 col-sm-6">
+							<div class="item">
+								<a href="">
+									<div class="img">
+										<img src="assets/images/produto.png">
+									</div><!-- img -->
+									<div class="content">
+										<h5>TIJOLO</h5>
+										<h2>9 X 14 X 19</h2>
+									</div><!-- content -->
+									<div class="conteudo">
+										<h4>Rendimento:</h4>
+										<ul>
+											<li>
+												<img src="assets/images/icone-rend1.png">
+												<p>33,4<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend2.png">
+												<p>50<br />pçs m²</p>
+											</li>
+											<li>
+												<img src="assets/images/icone-rend3.png">
+												<p>1,800<br />kg</p>
+											</li>
+										</ul>
+									</div><!-- conteudo -->
+								</a>
 							</div><!-- item -->
 						</div><!-- md-4 -->
 					</div><!-- row -->
